d577a15148437cf53046d871795ac00d71769731
Alexandre Narbonne <alexandre.narbonne@gadz.org>
2016-08-08 17:21:17 +0200

    remove useless methods in DeleteAccountMessageHandler

--- app/message_handlers/delete_user_message_handler.rb
+++ app/message_handlers/delete_user_message_handler.rb
@@ -29,55 +29,12 @@ class DeleteUserMessageHandler < BaseMessageHandler
        GoogleDirectoryDaemon.logger.error("Error when deleting #{gu.primary_email} : #{e.inspect}")
       raise_hardfail "Google apps error", error: e
     end
-
-
   end
 
-  # Expect data to contain :
-  #  -name
-  #  -password
-  #  -primary_email
-  def create_user data
-    if data.values_at(:name,:password,:primary_email).all?
-      unless GUser.find(data[:primary_email])
-        user=GUser.new
-        user.update!(**data)
-        user.save
-        puts " [x] User #{data[:primary_email]} created"
-      else
-        raise_softfail "Existing User"
-      end
-    else
-      raise_hardfail "Invalid Data"
-    end
-  end  
-
-  def set_uuid
-    @uuid=msg.data[:uuid]
-    unless @uuid
-      #TODO handle null hruid, maybe by validating the message JSON
-    end
-  end
-
-  def retrieve_gram_data
-    begin
-      #retrieve data from Gram, with password
-      GramV2Client::Account.find(@uuid, params:{show_password_hash: "true"})
-    rescue ActiveResource::ResourceNotFound
-      raise_gram_account_not_found(@uuid)
-    rescue ActiveResource::ServerError
-      raise_gram_connection_error
-    end
-  end
 
   def raise_google_account_not_found
     GoogleDirectoryDaemon.logger.error("Google account #{@key} does bot exist")
     raise_hardfail("Google account #{@key} does bot exist")
   end
-
-  def raise_no_gapps_email_error
-    GoogleDirectoryDaemon.logger.error("Accound #{@uuid} does bot have a Gapps email")
-    raise_hardfail("Accound #{@uuid} does bot have a Gapps email")
-  end
-
+  
 end
